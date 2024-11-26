16736afe7e8c39c7e80fa072f6013f1ed43bba31
gternovs <gternovs@akamai.com>
2024-11-26 12:10:49 +0200

    Removed unused files

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "galter-terraform-state-bucket"
-    key            = "root/root-terraform.tfstate"
-    region         = "il-central-1"
-    dynamodb_table = "terraform-lock"
-    encrypt        = true
-  }
-}
--- global/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "region" {
-  description = "AWS region for backend resources"
-  type        = string
-  default     = "il-central-1"  
-}
